c5066d26d55fccb1461974ae9a7e946ca7007d7b
Naveen Manikanta Kodigudla <naveenmanikanta111@gmail.com>
2017-04-17 00:51:19 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+  	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>  password strength </title>
+		 
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css" />		
+		<script src="script.js"></script>		
+	</head>
+	
+	<body>
+		<div id="container">
+		
+			<div id="header">
+				<h2>Password Strength Checking  <h2>
+			</div>
+			
+			<div id="content">	
+				<!-- form start-->
+				<form id="register">
+					<label for="password">Password : </label>
+					<input name="password" id="password" type="password"/>
+					<span id="result"></span>
+				</form>		
+				<!-- form end-->
+			</div>
+		</div>
+	</body>
+</html>
