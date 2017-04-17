a6b83d5e7ae7686665e89cfb45e68b77c131f0ef
Naveen Manikanta Kodigudla <naveenmanikanta111@gmail.com>
2017-04-17 22:57:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,27 +19,17 @@ $(document).ready(function()
 			$('#result').addClass('short')
 			return 'Too short' 
 		}
-		
-		//length is ok, lets continue.
-		
-		//if length is 8 characters or more, increase strength value
+ 
 		if (password.length > 7) strength += 1
-		
-		//if password contains both lower and uppercase characters, increase strength value
+ 
 		if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))  strength += 1
 		
-		//if it has numbers and characters, increase strength value
 		if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))  strength += 1 
 		
-		//if it has one special character, increase strength value
 		if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))  strength += 1
-		
-		//if it has two special characters, increase strength value
+ 
 		if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
-		
-		//now we have calculated strength value, we can return messages
-		
-		//if value is less than 2
+	 
 		if (strength < 2 )
 		{
 			$('#result').removeClass()
